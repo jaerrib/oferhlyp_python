4d535b82095f8b40b187a6643cdfa8ef360730f7
jaerrib <jaerrib@tutanota.com>
2024-04-13 10:40:55 -0400

    Add more game model tests

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -5,11 +5,71 @@ from game import Game
 
 class TestGame(unittest.TestCase):
 
+    def setUp(self):
+        self.game = Game()
+
     def test_new_game(self):
-        game = Game()
-        self.assertEqual(game.move_list, {})
-        self.assertEqual(game.result, "")
-        self.assertEqual(game.active_player, 1)
+        self.assertEqual(self.game.move_list, {})
+        self.assertEqual(self.game.result, "")
+        self.assertEqual(self.game.active_player, 1)
+
+    def test_get_check_positions(self):
+        self.assertEqual(
+            self.game.get_check_positions((4, 4)),
+            {
+                "adjacent": [
+                    (3, 3),
+                    (4, 3),
+                    (5, 3),
+                    (3, 4),
+                    (5, 4),
+                    (3, 5),
+                    (4, 5),
+                    (5, 5),
+                ],
+                "extended": [
+                    (2, 2),
+                    (4, 2),
+                    (6, 2),
+                    (2, 4),
+                    (6, 4),
+                    (2, 6),
+                    (4, 6),
+                    (6, 6),
+                ],
+                "jumpable": [],
+            },
+        )
+
+    def test_is_outside(self):
+        self.assertEqual(self.game.is_outside((10, 10)), True)
+        self.assertEqual(self.game.is_outside((4, 4)), False)
+
+    def test_get_available_moves(self):
+        self.assertEqual(
+            self.game.get_available_moves((0, 0)),
+            {
+                "possible_moves": [],
+                "possible_jumps": [(0, 2), (2, 2)],
+                "jumpable": [(0, 1), (1, 1)],
+            },
+        )
+        self.assertEqual(
+            self.game.get_available_moves((3, 6)),
+            {
+                "possible_moves": [],
+                "possible_jumps": [(1, 4), (3, 4), (5, 4)],
+                "jumpable": [(2, 5), (3, 5), (4, 5)],
+            },
+        )
+        self.assertEqual(
+            self.game.get_available_moves((3, 5)),
+            {
+                "possible_moves": [(2, 4), (3, 4), (4, 4)],
+                "possible_jumps": [],
+                "jumpable": [],
+            },
+        )
 
 
 if __name__ == "__main__":
