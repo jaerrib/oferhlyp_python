491c32b80b6237728dc28e589150a2d49c7c8f42
jaerrib <jaerrib@tutanota.com>
2024-05-20 18:23:16 -0400

    Add reset button and basic session data handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 
 from app.game import Game
 from app.game_loops import game_loop
@@ -9,9 +9,28 @@ app.secret_key = "dev"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if "data" in session:
+        session.pop("data")
+    if "data" not in session:
+        game = Game()
+        game.active_player = 2
+        session["data"] = {
+            "board": game.board.data,
+            "move_list": game.move_list,
+            "jumped_list": game.jumped_list,
+            "active_player": game.active_player,
+            "game_over": game.game_over,
+            "turn_over": game.turn_over,
+        }
+    return render_template("index.html", data=session["data"])
 
 
 @app.route("/process/<int:row>/<int:col>")
 def process(row, col):
-    pass
+    return redirect("/")
+
+
+@app.route("/reset")
+def reset():
+    session.pop("data")
+    return redirect("/")
