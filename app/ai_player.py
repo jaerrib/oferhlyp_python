d7e11eeb2daac39f220fd6eed00e210887c1e891
jaerrib <jaerrib@tutanota.com>
2024-12-20 12:24:51 -0500

    Handle chained computer attacks

--- app/ai_player.py
+++ app/ai_player.py
@@ -10,37 +10,45 @@ from app.game_loops import (
 
 
 def play_computer_move(data):
-    # Choose a token to move
-    token_list = []
-    for row in range(0, 7):
-        for col in range(0, 7):
-            if data["board"][row][col] != 0 and data["board"][row][col]["player"] == 2:
-                possible_moves = get_moves(data, (col, row))
+    # Choose a token to move if not actively jumping
+    if not data["actively_jumping"]:
+        token_list = []
+        for row in range(0, 7):
+            for col in range(0, 7):
                 if (
-                    len(possible_moves["possible_moves"]) > 0
-                    or len(possible_moves["possible_jumps"]) > 0
+                    data["board"][row][col] != 0
+                    and data["board"][row][col]["player"] == 2
                 ):
-                    token_list.append((col, row))
-    selected_token = random.choice(token_list)
-    data = select_token(data, row=selected_token[1], col=selected_token[0])
+                    possible_moves = get_moves(data, (col, row))
+                    if (
+                        len(possible_moves["possible_moves"]) > 0
+                        or len(possible_moves["possible_jumps"]) > 0
+                    ):
+                        token_list.append((col, row))
+        selected_token = random.choice(token_list)
+        data = select_token(data, row=selected_token[1], col=selected_token[0])
+    else:
+        selected_token = (data["active_col"], data["active_row"])
     # Determine moves available for that token
     possible_moves = get_moves(data, selected_token)
-    # move_choices = []
     move_choices = [
         item
         for sublist in possible_moves.values()
         if sublist != possible_moves["jumpable"]
         for item in sublist
     ]
-    selected_move = random.choice(move_choices)
+    if not data["actively_jumping"]:
+        selected_move = random.choice(move_choices)
+    else:
+        selected_move = random.choice(possible_moves["possible_jumps"])
     # Choose a move to play for that token
     if (
-        selected_move
-        in possible_moves["possible_moves"]
-        # and data["actively_jumping"] == False
+        selected_move in possible_moves["possible_moves"]
+        and data["actively_jumping"] == False
     ):
         data = assign_selected(data, row=selected_move[1], col=selected_move[0])
         data = turn_reset(data)
+        return data
     elif selected_move in possible_moves["possible_jumps"]:
         data = assign_jump(
             data,
@@ -48,13 +56,9 @@ def play_computer_move(data):
             col=data["active_col"],
             row=data["active_row"],
         )
-        data = turn_reset(data)  # remove after implementing continued jumping
-    # If jumping, decide whether to continue
-    # (
-    #     play_computer_move(data)
-    #     if data["actively_jumping"] and random.random() < 0.2
-    #     else turn_reset(data)
-    # )
+        # If jumping, decide whether to continue
+        if data["actively_jumping"] and random.random() > 0.2:
+            play_computer_move(data)
     return data
 
 
